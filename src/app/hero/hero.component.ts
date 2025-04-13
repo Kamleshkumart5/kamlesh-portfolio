71e6f967e31a12c699e7dae52c68047f9f4c9a77
Kamleshkumart5 <Kamleshkumart5@gmail.com>
2025-04-13 11:41:21 +0530

    Profile updated

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -14,6 +14,7 @@ export class HeroComponent {
   heroSubtitle = 'Technical Leader & Innovator';
   email = 'kamleshkumart5@gmail.com';
   phoneNumber = '9653410690';
+  Location = "Mumbai"
   // heroDescription = 'Experienced Technical Analyst and ERP Implementation Executive with a proven track record in API development, full-stack technologies, and leading educational ERP projects.';
   heroDescription = `Welcome to my portfolio! I'm passionate about building scalable solutions and leading teams to success. With expertise in .NET Core, Angular, and ERP implementations, I strive to deliver impactful results.`;
   constructor(private router: Router) { } // Inject Router
@@ -23,7 +24,12 @@ export class HeroComponent {
     console.log('View Projects clicked');
     // You can use Angular Router to navigate here
   }
-
+  viewExperience() {
+    // Implement navigation to projects section
+    this.router.navigate(['/experience']);
+    console.log('View experience clicked');
+    // You can use Angular Router to navigate here
+  }
   downloadResume() {
     window.open('/assets/resume.pdf', '_blank'); // Open resume in a new tab for download
   }
